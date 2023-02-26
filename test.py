add35da07abeeea605dcf5faaf5d392e28411b23
gogiddd <126254055+gogiddd@users.noreply.github.com>
2023-02-26 20:32:00 +0600

    Add files via upload

--- test.py
+++ test.py
@@ -7,11 +7,11 @@ import asyncio
 app = Client('session', api_id='29485755', api_hash='69ad4ad665440835b2c916a9ad9a6818')
 
 
-chat_id = "@anonkarubot"  #тут свой чат в котором спамить
+chat_id = "@datinganon_bot"  #тут свой чат в котором спамить
 
 @app.on_message(chat_id)
 async def spam(client: Client, message: types.Message):
-    if '/stop - завершить диалог' in message.text:  # Меняем это значение если хотим спамить в другом чате
+    if 'Вам найден собеседник, начинайте диалог.' in message.text:  # Меняем это значение если хотим спамить в другом чате
         sleep(1.8)
         await app.send_message(chat_id, "Дароу, заебал, чё как жизнь?")
         sleep(2)
@@ -19,15 +19,15 @@ async def spam(client: Client, message: types.Message):
 Зацени, бро, я с кентом вейпшоп открыл vape_shop123_BOT\n
 В подарок к первому заказу отсылают одноразку или жижу на выбор!)""")
 
-        if '/stop - завершить диалог' in message.text:
+        if 'Вам найден собеседник, начинайте диалог.' in message.text:
              sleep(1.7)
              await app.send_sticker(chat_id, "CAACAgIAAxkBAAEH4bJj-KAOgflfOVfI0gPVPwNIwTi2WAACgiQAAlh_yUuGunKPE1k7OS4E")
              sleep(2)
 
-             if "/stop - завершить диалог"  in message.text:  # Меняем это значение если хотим спамить в другом чате
+             if "Вам найден собеседник, начинайте диалог."  in message.text:  # Меняем это значение если хотим спамить в другом чате
                  await app.send_message(chat_id, "/stop")
                  sleep(1.5 )
-                 await app.send_message(chat_id, "Найти собеседника 🔎")
+                 await app.send_message(chat_id, "/next")
 
 
 app.run()
